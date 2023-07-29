f71d57ff39e1cd01c5160cecfbab04e362b19b06
szaruga92 <szaruga92@gmail.com>
2023-07-29 20:46:18 +0100

    Commit_V1

--- myapp/src/test/java/MyappApplicationTests.java
+++ myapp/src/test/java/MyappApplicationTests.java
@@ -1,15 +1,7 @@
-import com.szaruga.myapp.entity.Members;
 import com.szaruga.myapp.service.MemberServiceImpl;
-import com.szaruga.myapp.service.MembersService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @SpringBootTest
 class MyappApplicationTests {
