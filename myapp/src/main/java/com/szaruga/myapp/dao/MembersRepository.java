e1a3e6febc788aa0346e97a00eb963606f2992eb
szaruga92 <szaruga92@gmail.com>
2023-07-29 20:41:06 +0100

    Commit_V1

--- myapp/src/main/java/com/szaruga/myapp/dao/MembersRepository.java
+++ myapp/src/main/java/com/szaruga/myapp/dao/MembersRepository.java
@@ -3,7 +3,5 @@ package com.szaruga.myapp.dao;
 import com.szaruga.myapp.entity.Members;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface MembersRepository extends JpaRepository<Members, Integer> {
 }
